398ab062708100574fe8f7b654e89b68fd915f90
Stephan Vedder <stephan.vedder@gmail.com>
2017-10-01 17:27:49 +0200

    Preparations for mapped images

--- src/game/states/titlescreen.cpp
+++ src/game/states/titlescreen.cpp
@@ -8,15 +8,10 @@ arda::TitleScreen::TitleScreen(FileSystem& fs,Graphics& graphics) :
 m_graphics(graphics)
 {
 	auto& ren = m_graphics.GetRenderer();
-	auto stream = fs.GetStream("art/compiledtextures/ti/titlescreenuserinterface.jpg");
-	Image title_ui(stream);
-	auto tex = ren.CreateTexture(title_ui);
+	auto tex = m_graphics.GetTexture("titlescreenuserinterface",fs);
 	m_title_ring = m_graphics.CreateSprite(tex);
 
-	stream = fs.GetStream("art/compiledtextures/lo/load_w_ea.jpg");
-	Image title_ea(stream);
-
-	tex = ren.CreateTexture(title_ea);
+	tex = m_graphics.GetTexture("load_w_ea",fs);
 	m_title_ea = m_graphics.CreateSprite(tex);
 
 	ren.AddDrawable(m_title_ea);
